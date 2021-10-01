a0010d992425f80605ec87f647e0fd84c7a6d653
7hgTnec <7hgTnec@gmail.com>
2021-10-01 14:21:38 -0700

    Let screen can display the original acive and actually active. Add a manually control model

--- README.md
+++ README.md
@@ -2,20 +2,22 @@
 Colab for UCLA 209 AS Computational Robotics
 
 
-# for use
-make sure you installed the pygame modules
-Then just run simulator.py
+## for use
+    make sure you installed the pygame modules
+    Then just run simulator.py
+    The default model is Manually control, use up, down, left, right and RETURN key to move up, down, left, right and stay
+    Use key 'a' to switch between automatic/manually
 
-# config.txt
-first line is the (column number, row number)
-second line is obstacles coordinates
-third line is ice creams coordinates
-fourth line is the initial robot's position
-last line is the value of pe
+## config.txt
+    first line is the (column number, row number)
+    second line is obstacles coordinates
+    third line is ice creams coordinates
+    fourth line is the initial robot's position
+    last line is the value of pe
 
-# TODO
+## TODO
 1. finish the calculate of h and O
-2. Show more information on screen
-3. Add switch between robot auto/manual move
+2. Show more information on screen (part finished)
+3. ~~Add switch between robot auto/manual move (Finished)~~
 4. Add more comment
 5. formalize the code style
--- simulator.py
+++ simulator.py
@@ -11,31 +11,89 @@ from robot import Robot
 GRID_SIZE = 50
 LINE_SIZE = 3
 
+# this dict is used to map the arrow input to the robot move direction
+        # NoMove, Up, Down, Right, Left
+        #   0     1     2     3     4
+ROTATIONS = {pygame.K_RETURN: 0, pygame.K_UP: 1, pygame.K_DOWN: 2, pygame.K_RIGHT: 3, pygame.K_LEFT: 4}
+NUM2DIRECTION = {0:"NoMove", 1:"Up", 2:"Down", 3:"Right", 4:"Left"}
+
 # this simulator will let the robot running on the gridworld
 class Simulator:
     def __init__(self, gridWorld:Gridworld, width, height) -> None:
+        # construction function need one Gridworld object to simulate
+        # width and height is the width and height of the simulator window
+
+        self.width = width
+        self.height = height
+
+        # initial the simulator window configs
         pygame.init()
         pygame.display.set_caption("Simulator")
-        self.screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((width+200, height))
+
+        # set the simulator objects, include gridworld(Environment) and robot
         self.gridWorld = gridWorld
         self.robot = Robot()
+        self.isAuto = False             # this variable used as a flag to set is the robot take action automatic or manually
+                                        # by default, it's movement is be manually controlled
         
-    def loop(self) -> None:
-        while True:
-            self.draw_map()
+        # set the font
+        self.myFont = pygame.font.SysFont("arial", 20)
 
+    def loop(self) -> None:
+        actionText = "Original Action: None"
+        actualActionText = "Actually Action: None"
+
+        # this is the main loop that in a while loop to keep the simulator running
+        while True:
+            action = None                   # default direction is NoMove
+            actualAction = None             # to store the actual action after took an action
+
+            # each loop, the simulator redraw the whole screen to update the changes
+            self.drawScreen()
+
+            # then get event and process it
             for event in pygame.event.get():
+                # got an quit event, then close the simulator and exit the system
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-            decision = self.robot.makeDecision()
-            self.gridWorld.takeAction(decision, self.gridWorld.pe)
+
+                #TODO
+                # Add more hot key for this simulator
+
+                # then check is there any "KEYDOWN" events
+                if event.type == KEYDOWN:
+                    # "a" key is used to switch between auto/manual
+                    if event.key == K_a:
+                        self.isAuto = not self.isAuto
+
+                    # to check if is movement keys
+                    if self.isMovementKeys(event.key):
+                        action = ROTATIONS[event.key]
+
+            if self.isAuto:
+                action = self.robot.makeDecision()
+                _, actualAction = self.gridWorld.takeAction(action, self.gridWorld.pe)
+                actionText = "Original Action: " + NUM2DIRECTION[action]
+                actualActionText = "Actually Action: " + NUM2DIRECTION[actualAction]
+            else:
+                if action != None:
+                    _, actualAction = self.gridWorld.takeAction(action, 0)
+                    actionText = "Original Action: " + NUM2DIRECTION[action]
+                    actualActionText = "Actually Action: " + NUM2DIRECTION[actualAction]
+
+            self.screen.fill(pygame.Color("black"), (self.width+5,0, self.width+200, self.height))
+            self.screen.blit(self.myFont.render(actionText, True, pygame.Color("white"), pygame.Color("black")), (self.width+10, 5))
+            self.screen.blit(self.myFont.render(actualActionText, True, pygame.Color("white"),pygame.Color("black")), (self.width+10, 55))
+
             self.gridWorld.updateMap(0)
+
             # this is used to control the simulate speed
-            pygame.time.wait(50)
+            pygame.time.wait(10)
             pygame.display.update()
 
-    def draw_map(self) -> None:
+    def drawScreen(self) -> None:
         map = self.gridWorld.getMap()
         cNum = self.gridWorld.cNum
         rNum = self.gridWorld.rNum
@@ -50,7 +108,7 @@ class Simulator:
                 # calculate the rectangle
                 # and do an matrix transform
                 x = i
-                px = i*GRID_SIZE
+                px = x*GRID_SIZE
                 y = rNum - j - 1
                 py = y*GRID_SIZE
 
@@ -70,6 +128,10 @@ class Simulator:
             pygame.draw.line(self.screen, pygame.Color("black"), (i*GRID_SIZE,0), (i*GRID_SIZE, rNum*GRID_SIZE), 3)
         for j in range(rNum):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, j*GRID_SIZE), (cNum*GRID_SIZE, j*GRID_SIZE), 3)
+        pygame.display.update()
+
+    def isMovementKeys(self, k) -> bool:
+        return(k==pygame.K_RETURN or k == pygame.K_UP or k == pygame.K_DOWN or k == pygame.K_LEFT or k == pygame.K_RIGHT)
 
 if __name__ == '__main__':
 
@@ -104,7 +166,7 @@ if __name__ == '__main__':
             elif 4 == lineNumber:
                 robot_pos = (numbers[0], numbers[1])
             else:
-                pe = numbers[0]
+                pe = float(line)
 
     # TODO explan more 
     gridworld = Gridworld(cNum, rNum, obstacles_list, IC_list, robot_pos, pe)
