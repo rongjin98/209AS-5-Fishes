953625145ff99ec56acc3fb321d237dd93c2d5fa
choky <shenmeguislb@gmail.com>
2021-10-05 17:52:43 -0700

    implement createProb

--- gridworld.py
+++ gridworld.py
@@ -1,5 +1,13 @@
+import numpy as np
+
+
 class GridWorld:
     def __init__(self):
+        self.pe = .25
+        self.numActions = 5
+        self.gridSize = 5
+        self.block = ((1,1), (2,1), (1,3), (2,3))
+        
         self.state = self.createState
         self.action = self.createAction
         self.probability = self.createProbability
@@ -11,7 +19,26 @@ class GridWorld:
     
     @property
     def createProbability(self):
-        NotImplemented
+        '''
+        direction:
+        1 2 3 4 5
+        left right up down stay
+        '''
+        gridSize = self.gridSize
+        numActions = self.numActions
+        actionProb = 1 / numActions
+        block = self.block
+        
+        # initialize grid
+        grid = np.array([[[actionProb for _ in range(numActions)] for _ in range(gridSize)] for _ in range(gridSize)])
+        
+        # assgin block
+        for i,j in block:
+            for z in range(numActions):
+                print(z)
+                grid[z, i,j] = 0
+        
+        return grid
     
     @property
     def createAction(self):
@@ -20,4 +47,17 @@ class GridWorld:
     @property
     def createObservation(self):
         NotImplemented
-        
+    
+    def todo(self, *args, **kwargs):
+        print("NotImplemented")
+    
+    
+    
+if __name__ == "__main__":
+    """
+    testing GridWorld
+    """
+    
+    grid = GridWorld()
+    print(grid.probability)
+    
